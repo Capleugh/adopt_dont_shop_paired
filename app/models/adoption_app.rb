45ad040a81cfab35d950955c72c1559933edad46
Joshua O'bannon <jtobannon@gmail.com>
2019-12-09 14:36:55 -0700

    Complete relationships and validations for pet, application, and join table connnecting resources
    Co-Authored-By: Carleigh Crockett <Capleugh@users.noreply.github.com>

--- app/models/adoption_app.rb
+++ app/models/adoption_app.rb
@@ -1,3 +1,6 @@
 class AdoptionApp < ApplicationRecord
+  validates_presence_of :name, :address, :city, :state, :zip, :phone, :description
+
   has_many :adoption_app_pets
+  has_many :pets, through: :adoption_app_pets
 end
